2a1be9764cb7acd82af1416e225ab439af7ac984
Steven Johnson <johnson.stevenwilliam@gmail.com>
2024-07-14 20:47:37 -0500

    Added post mapping for processing the order.

--- src/main/java/com/stevenjohnson/tacocloud/controllers/OrderController.java
+++ src/main/java/com/stevenjohnson/tacocloud/controllers/OrderController.java
@@ -1,11 +1,14 @@
 package com.stevenjohnson.tacocloud.controllers;
 
+import com.stevenjohnson.tacocloud.domain.TacoOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
 @RequestMapping("/orders")
@@ -16,5 +19,12 @@ public class OrderController {
     @GetMapping("/current")
     public String orderForm() {
         return "order";
+    }
+
+    @PostMapping()
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+        logger.info("Order was submitted: {}", order);
+        sessionStatus.setComplete();
+        return "redirect:/";
     }
 }
